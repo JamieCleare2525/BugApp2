0fc21f72b3b0f8d773ba1da1af726dafa135e101
Jamie <up733026@myport.ac.uk>
2016-07-13 10:09:31 +0100

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,13 +6,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Bug Reporting App"
   end
 
-  test "should get root" do
-    get static_pages_home_url
-    assert_response :success
-  end
-
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Home | #{@base_title}"
   end
