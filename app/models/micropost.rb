b6a3659535f92baddf4a1b82b3f30ce15c45c030
Jamie <up733026@myport.ac.uk>
2016-07-13 13:33:28 +0100

    Finish layout and routes

---
+++ app/models/micropost.rb
@@ -0,0 +1,7 @@
+class Micropost < ApplicationRecord
+  belongs_to :user
+  validates :user_id, presence: true
+  validates :title, presence: true, length: { maximum: 40 }
+  validates :description, presence: true, length: { maximum: 255 }
+
+end
