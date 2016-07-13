d73f2c38a9f00465101edd5b250d48508ef08869
Jamie <up733026@myport.ac.uk>
2016-07-13 09:19:36 +0100

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    root 'application#hello'
+    render html: "hello world!"
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,20 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+
+  def setup
+    @base_title = "Bug Reporting App"
+  end
+
+  test "should get root" do
     get static_pages_home_url
     assert_response :success
   end
 
-  test "should get help" do
-    get static_pages_help_url
+  test "should get home" do
+    get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
 end
