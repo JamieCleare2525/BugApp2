d30d9af8fa49d59058557690af9457f7576609fb
Jamie <up733026@myport.ac.uk>
2016-07-14 10:30:51 +0100

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -32,4 +32,9 @@ module SessionsHelper
     session[:forwarding_url] = request.original_url if request.get?
   end
 
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+
 end
