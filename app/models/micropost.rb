738aaa46d7c7d3172ba6b5c2fc0945dc3c47e097
Jamie <up733026@myport.ac.uk>
2016-07-14 11:52:05 +0100

    Micropost-revised

--- app/models/micropost.rb
+++
@@ -1,7 +0,0 @@
-class Micropost < ApplicationRecord
-  belongs_to :user
-  validates :user_id, presence: true
-  validates :title, presence: true, length: { maximum: 40 }
-  validates :description, presence: true, length: { maximum: 255 }
-
-end
